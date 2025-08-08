81d20e1d7e25cf8c0cf452b16948d046151e5d86
Sharrrkkk <projectwebmaster3@gmail.com>
2025-08-07 22:56:05 -0400

    I have created the structure of a new project, a library package for game solvers. In this case, we have started by reusing a lib from a project I created called wordgamesolver for this project, but I have improved the lib to make it special as a lib for a library package for game solvers. Creating directories, files, configurations, repository, tests.

---
+++ src/gamesolverlib/__about__.py
@@ -0,0 +1,24 @@
+__all__ = ["__title__", "__version__", "__summary__", "__description__", "__url__",
+         "__docs_url__", "__issues_url__", "__status__", "__keywords__",
+           "__author__", "__maintainer__", "__license__", "__copyright__"]
+
+
+__title__ = "gamesolverlib"
+__version__ = "1.0.0"
+__summary__ = "A solver for games."
+__description__ = ("Currently, only the **Word Finder** solver is implemented."
+                   " It searches for all possible words of a specific length" 
+                   " that can be formed using only the letters or letter" 
+                   " combinations provided by the user.")
+__url__ = "https://github.com/Sharrrkkk/game_solver_library"
+__docs_url__ = "https://github.com/Sharrrkkk/game_solver_library#README.md"
+__issues_url__ = "https://github.com/Sharrrkkk/game_solver_library/issues"
+__status__ = "Production/Stable"
+__keywords__ = "anagram, anagrams, game, games, generate, word, words, solver, finder"
+__author__ = "Sharrrkkk"
+__maintainer__ = "Sharrrkkk"
+__license__ = "MIT"
+__copyright__ = "2025 Sharrrkkk"
+
+
+
---
+++ src/gamesolverlib/__init__.py
@@ -0,0 +1,2 @@
+from .wordfinder_lib.wordfinder import WordFinder
+from .__about__ import *
